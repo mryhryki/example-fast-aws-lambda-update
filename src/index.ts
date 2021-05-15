bc1342011a2f189c16a1aee9bf88f4e03bf1e73c
mryhryki <mryhryki@gmail.com>
2021-05-15 15:43:36 +0900

    Use AWS S3.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import { S3 } from "aws-sdk";
+const s3 = new S3({ apiVersion: "2006-03-01" });
+const Bucket = "mryhryki-temp";
+
 export const handler = async (): Promise<any> => {
+  const result = await s3.listObjectsV2({ Bucket, Prefix: "", MaxKeys: 1000 }).promise();
+
   return {
     statusCode: 200,
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({success: "BBBB"}, null, 2)
+    body: JSON.stringify(result, null, 2)
   }
 }
