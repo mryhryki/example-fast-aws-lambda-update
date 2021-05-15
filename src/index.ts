94ec6c34f12d7b5fdfc532765900421a9bc5b396
mryhryki <mryhryki@gmail.com>
2021-05-15 15:46:47 +0900

    Use ap-northeast-1 region.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { S3 } from "aws-sdk";
 const s3 = new S3({ apiVersion: "2006-03-01" });
-const Bucket = "mryhryki-temp";
+const Bucket = "b1ec0be3-01c8-438a-817f-710e84866464";
 
 export const handler = async (): Promise<any> => {
   const result = await s3.listObjectsV2({ Bucket, Prefix: "", MaxKeys: 1000 }).promise();
